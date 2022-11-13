c9667f88dc872b7ffb6180309bb14befb75d8c54
Juan Calero <marcelocalero5@gmail.com>
2022-11-12 21:21:55 -0300

    bug fixes in client and user modules

--- src/modules/client/client.controller.ts
+++ src/modules/client/client.controller.ts
@@ -1,44 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseGuards } from '@nestjs/common';
-import { ClientService } from './client.service';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, HttpCode, HttpStatus } from '@nestjs/common';
+import { ClientService } from 'src/modules/client/client.service';
 import { CreateClientDto, UpdateClientDto } from 'src/modules/client/dto';
-import { JwtAuthGuard } from '../auth/jwt';
 
-
-@UseGuards(JwtAuthGuard)
 @Controller('client')
 export class ClientController {
   constructor(private readonly clientService: ClientService) { }
+
   @Post('/create')
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createClientDto: CreateClientDto) {
     return this.clientService.create(createClientDto);
   }
 
   @Get('/')
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.clientService.findAll();
   }
 
   @Get('/:id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id', ParseIntPipe) id_client: number) {
     return this.clientService.findOne(+id_client);
   }
 
   @Patch('update/:id')
+  @HttpCode(HttpStatus.ACCEPTED)
   update(@Param('id', ParseIntPipe) id_client: number, @Body() updateClientDto: UpdateClientDto) {
     return this.clientService.update(+id_client, updateClientDto);
   }
 
   @Patch('archive/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   archive(@Param('id', ParseIntPipe) id_client: number) {
     return this.clientService.archive(+id_client);
   }
 
   @Patch('unarchive/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   unarchive(@Param('id', ParseIntPipe) id_client: number) {
     return this.clientService.unarchive(+id_client);
   }
 
   @Delete('remove/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id', ParseIntPipe) id_client: number) {
     return this.clientService.remove(+id_client);
   }
--- src/modules/client/client.module.ts
+++ src/modules/client/client.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ClientService } from 'src/modules/client/client.service';
 import { ClientController } from 'src/modules/client/client.controller';
+import { ClientService } from 'src/modules/client/client.service';
 import { Client } from 'src/modules/client/client.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      Client
-    ])
-  ],
+  imports: [TypeOrmModule.forFeature([Client])],
   controllers: [ClientController],
   providers: [ClientService]
 })
