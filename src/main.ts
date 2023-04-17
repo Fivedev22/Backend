eaf798add50cd5322fe734e362fdab9e61cd1098
juancalero <marcelocalero5@gmail.com>
2023-04-16 21:29:40 -0300

    sufix API delete

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,16 @@ import { AppModule } from 'src/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transformOptions: {
-      enableImplicitConversion: true
-    }
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(process.env.PORT);
 }
 
